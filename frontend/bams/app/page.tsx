78e3e067903cd23de0a65111b6ea868d0a0f6962
Nihaal Nz <nihaalnz@gmail.com>
2023-11-30 03:43:42 +0400

    feat(frontend): init nextjs & shadcn
    Initialize NextJS (ts, tailwind) with shadcn

---
+++ frontend/bams/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='font-bold text-4xl'>Best Attendance Management System</h1>
+    </main>
+  )
+}
